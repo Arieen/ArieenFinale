a47171c2cb9d2ed02956f284e7cc8f2ad8c40ad7
JoseAngelB <al315234@uji.es>
2017-11-17 20:32:39 +0100

    cambio de sprites y de mundo
    aproximacion...

---
+++ Assets/Scripts/Arieen/ElegirMundo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElegirMundo : MonoBehaviour {
+
+	private bool esFantasia = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool isFantasia() {
+		return esFantasia;
+	}
+
+	public void isFantasia(bool fantasia) {
+		esFantasia = fantasia;
+	}
+}
