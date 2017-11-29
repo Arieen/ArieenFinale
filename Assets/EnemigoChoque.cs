17ec009f57850da6fd4571e3f4e4effff3260b4e
sevalper <33629147+sevalper@users.noreply.github.com>
2017-11-29 23:03:38 +0100

    Enemigos movimiento y su daño

---
+++ Assets/EnemigoChoque.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemigoChoque : MonoBehaviour {
+
+	public Sprite[] vidas;
+	PersonajeCOntrol personajeControl;
+	public Image imagenvidas;
+	bool vulnerable;
+	public float tiempoInvulnerable;
+	public Color colorTransp;
+	public Color colorNoTransp;
+
+	// Use this for initialization
+	void Start () {
+		personajeControl = GetComponent<PersonajeCOntrol> ();
+		vulnerable = true;
+	}
+
+	void Transparencia(bool transparente)
+	{
+		if (transparente)
+			GetComponent<SpriteRenderer> ().color = colorTransp;
+		else
+			GetComponent<SpriteRenderer> ().color = colorNoTransp;
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Enemigo" && vulnerable) {
+			personajeControl.currhealth--;
+			Invulnerable ();
+		}
+	}
+
+	void Invulnerable()
+	{
+		StartCoroutine (InvulnerableIEnume ());
+	}
+
+	IEnumerator InvulnerableIEnume()
+	{
+		vulnerable = false;
+		Transparencia (true);
+		yield return new WaitForSeconds (tiempoInvulnerable);
+		vulnerable = true;
+		Transparencia (false);
+	}
+}
---
+++ Assets/movimientoEnemigo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoEnemigo : MonoBehaviour {
+
+	Animator animator;
+	bool direccion;
+	public float velocidad;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direccion)
+			this.transform.Translate (Vector2.left * velocidad * Time.deltaTime);
+		else 
+			this.transform.Translate (Vector2.right * velocidad * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "TopeEnemigo") {
+			direccion = !direccion;
+			Vector3 vectorContrario = transform.localScale;
+			vectorContrario.x *= -1;
+
+			transform.localScale = vectorContrario;
+
+			Debug.Log ("tocado");
+		}
+	}
+}
