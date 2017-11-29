5ba36f9e7d8ea1d462bcea049a46a0ff8a649d5d
sevalper <33629147+sevalper@users.noreply.github.com>
2017-11-29 21:14:09 +0100

    Fragmentos y contador

--- Assets/Scripts/HUD/Fragmentos.cs
+++ Assets/Scripts/HUD/Fragmentos.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Fragmentos : MonoBehaviour {
-    public int fragmentostotales = 10;
-    public int fragmentosrecogidos = 0;
+	public int fragmentostotales;
+	public int fragmentosrecogidos;
     public Text marcador;
 	// Use this for initialization
-	void Start () {
-        actualizarmarcador();
-    }
 
-    void actualizarmarcador()
-    {
-        marcador.text = "" + fragmentosrecogidos + "/" + fragmentostotales;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        actualizarmarcador();
+	void Awake()
+	{
+		marcador.text = "" + Convert.ToString (fragmentosrecogidos) + "/" + Convert.ToString (fragmentostotales);
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Fragmentos") {
+			fragmentosrecogidos++;
+			marcador.text = "" + Convert.ToString (fragmentosrecogidos) + "/" + Convert.ToString (fragmentostotales);
+			Destroy (other.gameObject);
+			Debug.Log("tocado");
+		}
+	}
+   
 }
